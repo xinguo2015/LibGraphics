0c21ea752e673ed2ee4795f5c6f1d060f9933ce2
Xinguo Liu <xgliu@cad.zju.edu.cn>
2017-06-11 16:10:28 +0800

    init

---
+++ src/demo.c
@@ -0,0 +1,26 @@
+
+
+#define Demo_ID 1
+
+#if Demo_ID == 1
+#include "house.c"
+
+#elif Demo_ID == 2
+#include "kochsnow.c"
+
+#elif Demo_ID == 3
+#include "igp.c"
+
+#elif Demo_ID == 4
+#include "igp_char.c"
+
+#elif Demo_ID == 5
+#include "igp_keyboard.c"
+
+#elif Demo_ID == 6
+#include "igp_mouse.c"
+
+#elif Demo_ID == 7
+#include "igp_timer.c"
+
+#endif 
